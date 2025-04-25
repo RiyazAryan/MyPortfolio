e0b81249ce318fbb580b90ce914be641cfa36934
Riyaz Basha Shaik <130349481+RiyazAryan@users.noreply.github.com>
2025-04-25 17:36:04 +1000

    Final portfolio with dark mode fix and README

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -64,6 +64,15 @@ export function Home(){
       <FadeInSection>
       <Element name="contact" className="h-fill"> <ContactSection /> </Element>
       </FadeInSection>
-    </div>
+      <div className="flex justify-center p-10">
+                <div className="text-[#4F4f4F] pr-2">
+                  Made by
+                </div>
+                <div className="font-semibold">
+                  Riyaz Basha
+                </div>
+                <img src="../Riyaz.ico" alt="icon" className="w- h-5 mr-2"></img>
+      </div>
+      </div>
     )
 }
